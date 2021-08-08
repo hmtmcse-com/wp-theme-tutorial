f87c236b722d1933d6d6c5727f00b0ee85ffb8e7
hmtmcse <hmtm.cse@gmail.com>
2021-08-08 12:05:08 +0600

    WordPress theme include CSS, JavaScript & other files

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function add_files_into_theme() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style( 'design', get_template_directory_uri() . '/assets/css/design.css',false,'1.1','all');
-    wp_enqueue_script( 'app', get_template_directory_uri() . '/assets/js/app.js', [], 1.1, true);
+    wp_enqueue_script( 'app', get_template_directory_uri() . '/assets/js/app.js', [], 1.1, false);
 }
 add_action( 'wp_enqueue_scripts', 'add_files_into_theme' );
 
